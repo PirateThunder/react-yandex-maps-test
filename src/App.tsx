4a9b987259b9e63d98489c62487856bf47c7b0de
ilyakhakimov03 <Khakimov.IA@servionica.ru>
2024-11-16 01:04:11 +0500

    m

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { YMaps, Map, Placemark, Clusterer } from '@pbe/react-yandex-maps';
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 const clusterPoints = [
   [55.684758, 37.738521],
@@ -16,23 +16,31 @@ const clusterPoints = [
 const App = () => {
   const [zoom, setZoom] = useState(9);
   const mapState = useMemo(
-    () => ({ center: [55.75, 37.57], zoom, controls: ["zoomControl", "fullscreenControl"] }),
+    () => ({ center: [55.75, 37.57], zoom }),
     [zoom]
   );
+
+  const click = () => {
+    console.log('click')
+    // setZoom(3) прелыдущий зум динамически не обновляется в mapState. как получать актуальные данные от карты в реальном врвемени?
+    setZoom((zoom) => (zoom === 9 ? 12 : 9))
+  }
+
+  useEffect(() => {
+    console.log('zoom', mapState)
+  }, [zoom])
+
+  useEffect(() => {
+    console.log('mapState', mapState)
+  }, [mapState])
 
   return (
     <YMaps>
       <div>
         {/* {zoom} */}
-        <button onClick={() => setZoom(1)}>Zoom = 1</button>
+        <button onClick={click}>Zoom = 1</button>
         <Map height={'75vh'} width={'75wh'}
-          modules={["control.ZoomControl", "control.FullscreenControl"]}
           state={mapState}
-          // defaultState={{
-          //   center: [55.75, 37.57],
-          //   zoom: 9,
-          //   controls: ["zoomControl", "fullscreenControl"],
-          // }}
         >
           {/* <Placemark geometry={[55.684758, 37.738521]} /> */}
           <Clusterer
